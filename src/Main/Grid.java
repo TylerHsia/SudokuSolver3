b5c45edd2b878c0f911753c3951d3893f9eabc7a
Tyler Hsia <TylerHsia@gmail.com>
2022-04-12 20:53:53 -0700

    added accessors to get the set of values in a section, and to get the number of each candidate in a section to Grid.java

--- src/Main/Grid.java
+++ src/Main/Grid.java
@@ -4,10 +4,11 @@ import java.util.*;
 
 public class Grid {
 
-    //make iterators for row, column, or box?
-
     private final Cell[] cells;
 
+    /**
+     * Constructs a new empty sudoku grid
+     */
     public Grid(){
         cells = new Cell[81];
         for(int i = 0; i < cells.length; i++){
@@ -22,7 +23,7 @@ public class Grid {
      * @param value the value to be solved
      */
     public void solveCell(int row, int column, int value){
-        cells[row * 9 + column].solve(value);
+        getCell(row, column).solve(value);
     }
 
     /**
@@ -32,8 +33,29 @@ public class Grid {
      * @return the set of candidates at this position
      */
     public Set<Integer> getCands(int row, int column){
-        return cells[row * 9 + column].getCandidates();
+        return getCell(row, column).getCandidates();
     }
+
+    /**
+     * Returns the value of the cell at the given row and column
+     * @param row the row of the cell to be solved
+     * @param column the column of the cell to be solved
+     * @return the value of the cell at this position
+     */
+    public int getVal(int row, int column){
+        return getCell(row, column).getVal();
+    }
+
+    /**
+     * Returns whether the cell is solved at the given row and column
+     * @param row the row of the cell to be solved
+     * @param column the column of the cell to be solved
+     * @return the value of the cell at this position
+     */
+    public boolean isSolved(int row, int column){
+        return getCell(row, column).isSolved();
+    }
+
 
     /**
      * Removes value from this cell's candidates
@@ -43,12 +65,153 @@ public class Grid {
      * @return true iff this cell's candidates contained value
      */
     public boolean remove(int row, int column, int value){
-        return cells[row * 9 + column].remove(value);
+        return getCell(row, column).remove(value);
     }
 
-    //issolved, getval, size
+    /**
+     * Returns the set of all solved values in a given row
+     * @param row the row to be looked at
+     * @return the set of all solved values in a given row
+     */
+    public Set<Integer> getRow(int row){
+        return getVals(getRowCells(row));
+    }
 
+    /**
+     * Returns the set of all solved values in a given column
+     * @param column the column to be looked at
+     * @return the set of all solved values in a given column
+     */
+    public Set<Integer> getColumn(int column){
+        return getVals(getColumnCells(column));
+    }
 
+    /**
+     * Returns the set of all solved values in a given box
+     * @param row a row in the box to be looked at
+     * @param column a column in the box to be looked at
+     * @return the set of all solved values in a given box
+     */
+    public Set<Integer> getBox(int row, int column){
+        return getVals(getBoxCells(row, column));
+    }
 
+    /**
+     * Returns all of the non -1 values of getVal for each cell in cells
+     * @param cells the list of cells to return the values of
+     * @return a set of getVal for each cell in cells (-1 excluded)
+     */
+    private Set<Integer> getVals(List<Cell> cells){
+        Set<Integer> vals = new HashSet<>();
+        for(Cell cell: cells){
+            vals.add(cell.getVal());
+        }
+        vals.remove(-1);
+        return vals;
+    }
 
+    /**
+     * Returns a map of each value 1-9 mapped to the number of times it appears
+     * as a candidate in the given row
+     * @param row the row to be looked at whose candidates will be summed
+     * @return a map of 1-9 to the number of times they are candidates in the row
+     */
+    public Map<Integer,Integer> numCandsRow(int row){
+        return getCands(getRowCells(row));
+    }
+
+    /**
+     * Returns a map of each value 1-9 mapped to the number of times it appears
+     * as a candidate in the given column
+     * @param column the column to be looked at whose candidates will be summed
+     * @return a map of 1-9 to the number of times they are candidates in the column
+     */
+    public Map<Integer, Integer> numCandsColumn(int column){
+        return getCands(getColumnCells(column));
+    }
+
+    /**
+     * Returns a map of each value 1-9 mapped to the number of times it appears
+     * as a candidate in the given box
+     * @param row a column in the box to be looked at
+     * @param column a column in the box to be looked at
+     * @return a map of 1-9 to the number of times they are candidates in the box
+     */
+    public Map<Integer, Integer> numCandsBox(int row, int column){
+        return getCands(getBoxCells(row, column));
+    }
+
+    /**
+     * Returns a map of each value 1-9 mapped to the number of times it appears
+     * as a candidate in all of the given cells. If a cell only has one candidate,
+     * that candidate is includes in the sums
+     * @param cells the list of cells whose candidates will be summed
+     * @return a map of 1-9 to the number of times they are candidates in cells
+     */
+    private Map<Integer, Integer> getCands(List<Cell> cells){
+        Map<Integer, Integer> cands = new HashMap<>(9);
+        for(int i = 1; i <= 9; i++){
+            cands.put(i, 0);
+        }
+        for(Cell cell: cells){
+            for(int cand: cell.getCandidates()){
+                cands.put(cand, cands.get(cand) + 1);
+            }
+        }
+        return cands;
+    }
+
+    /**
+     * Returns the list of all cells in a given row
+     * @param row the row of the cells to be returned
+     * @return the list of all cells in a given row
+     */
+    private List<Cell> getRowCells(int row){
+        List<Cell> rCells = new ArrayList<>();
+        for(int c = 0; c < 9; c++){
+            rCells.add(getCell(row, c));
+        }
+        return rCells;
+    }
+
+    /**
+     * Returns the list of all cells in a given column
+     * @param column the column of the cells to be returned
+     * @return the list of all cells in a given column
+     */
+    private List<Cell> getColumnCells(int column){
+        List<Cell> cCells = new ArrayList<>();
+        for(int r = 0; r < 9; r++){
+            cCells.add(getCell(r, column));
+        }
+        return cCells;
+    }
+
+    /**
+     * Returns the list of all cells in a given box
+     * @param row a row in the box to be looked at
+     * @param column a column in the box to be looked at
+     * @return the list of all cells values in a given box
+     */
+    private List<Cell> getBoxCells(int row, int column){
+        List<Cell> bCells = new ArrayList<>();
+        int rX = row / 3;
+        int cX = column / 3;
+        for (int r = rX * 3; r < rX * 3 + 3; r++) {
+            for (int c = cX * 3; c < cX * 3 + 3; c++) {
+                bCells.add(getCell(r, c));
+            }
+        }
+        return bCells;
+    }
+
+    /**
+     * Returns the Cell at the given position in this
+     * @param row the row of the Cell to be returned
+     * @param column the column of the Cell to be returned
+     * @return the Cell at the given position in this
+     */
+    private Cell getCell(int row, int column){
+        return cells[row * 9 + column];
+    }
 }
